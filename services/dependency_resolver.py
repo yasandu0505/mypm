a3a3b9b75e9a032e64224d1115b37192e5a2cdb6
yasandu0505 <yasanduyaka2005@gmail.com>
2026-07-12 17:02:14 +0530

    feat  : comment update

--- services/dependency_resolver.py
+++ services/dependency_resolver.py
@@ -1,7 +1,7 @@
 class DependencyResolver:
     def __init__(self, dependencies):
         self.dependencies = dependencies
-
+    # TODO : modify this function to extract all the dependencies recursively for each package
     def resolve(self) -> list:
         print(f"Resolving dependencies -- {self.dependencies}")
         return self.dependencies
