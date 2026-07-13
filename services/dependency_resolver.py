29619f7d4236304d6a6a217886c1815ace4979a2
yasandu0505 <yasanduyaka2005@gmail.com>
2026-07-13 17:23:55 +0530

    fix : fixing bugs

--- services/dependency_resolver.py
+++ services/dependency_resolver.py
@@ -8,6 +8,8 @@ class DependencyResolver:
         print("\n")
         found_dependency = self.repository.find_package(package_name=dependency)
         if not found_dependency["dependencies"]:
+            if dependency not in self.final_dependency_list:
+                self.final_dependency_list.append(dependency)
             print("no more sub dependencies.....")
             print("\n")
             return
@@ -15,12 +17,15 @@ class DependencyResolver:
         for dependencyy in found_dependency["dependencies"]:
             self.final_dependency_list.append(dependencyy)
             self.find_dependencies(dependency=dependencyy)
-        self.final_dependency_list.append(dependency)
+        # self.final_dependency_list.append(dependency)
 
     # TODO : modify this function to extract all the dependencies recursively for each package
     def resolve(self) -> list:
         print("\n")
         print(f"Resolving dependencies --------- {self.dependencies}")
+        
         for dependency in self.dependencies:
             self.find_dependencies(dependency=dependency)
+            if dependency not in self.final_dependency_list:
+                self.final_dependency_list.append(dependency)
         return self.final_dependency_list
